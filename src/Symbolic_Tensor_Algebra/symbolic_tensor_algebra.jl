c8c98c7ee64a3404e27eda41b2d443571198e15d
OA <ahrend@mbd.rwth-aachen.de>
2026-05-13 18:13:53 +0200

    wip

--- src/Symbolic_Tensor_Algebra/symbolic_tensor_algebra.jl
+++ src/Symbolic_Tensor_Algebra/symbolic_tensor_algebra.jl
@@ -1,11 +1,28 @@
 
-function _promote_shape_2D(a::Symbolics.Arr{T,2}, b::Symbolics.Arr{T2,2}) where {T, T2}
-    sa = size(a)
-    sb = size(b)
-    return (1:sa[1]*sb[1], 1:sa[2]*sb[2])
+function _promote_shape_nD(a::Symbolics.Arr{T}, b::Symbolics.Arr{T2}) where {T, T2}
+    sizea = size(a)
+    sizeb = size(b)
+    dimsa = length(sizea)
+    dimsb = length(sizeb)
+    dimsout, dimsother, sizesmore = dimsa>=dimsb ? (dimsa, dimsb, sizea) : (dimsb, dimsa, sizeb)
+
+    sout = zeros(Int, dimsout)
+    for i in 1:dimsother
+        sout[i] = sizea[i] * sizeb[i]
+    end
+    for i in dimsout-dimsother+1:dimsout
+        sout[i] = sizesmore[i]
+    end
+
+    return Tuple(sout)
 end
 
-⊗(a::Symbolics.Arr, b::Symbolics.Arr) = SymbolicUtils.term(⊗, a, b; type=Symbolics.Arr{promote_type(eltype(a), eltype(b)),2}, shape=_promote_shape_2D(a, b))
+⊗(a::Symbolics.Arr{T}, b::Symbolics.Arr{T2}) where {T, T2} = begin
+    out_shape = _promote_shape_nD(a, b)
+    #out_type = Symbolics.Arr{promote_type(eltype(a), eltype(b)),length(out_shape)}
+    out_type = SymbolicUtils.promote_symtype(*, eltype(a), eltype(b))
+    return SymbolicUtils.term(⊗, a, b; type=out_type, shape=Tuple(1:o for o in out_shape))
+end
 
 # import Base.:*
 # Base.:*(a::SymbolicUtils.BSImpl.Type, b::SymbolicUtils.BSImpl.Type) = SymbolicUtils.term(*, a, b)
